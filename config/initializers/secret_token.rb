41433300f1d4a48fcc94d4912c2784da09435034
Daniel Chiu <dwchiu@stanford.edu>
2012-07-21 14:43:28 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedMeRails::Application.config.secret_token = '05f0688cadea6d330907bf94dd2093db4228b7e77bab430d1b4c5f96d2faffb6e985b79196dea1dfc8bfe6ccf83b6968023dac6a1b9c04e050479617532e9e02'
