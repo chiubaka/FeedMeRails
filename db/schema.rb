14df982f052609f45d107b16ba693c8e114a8fb0
Edward Lando <edwardlando8@gmail.com>
2012-07-21 15:39:42 -0700

    added extra attributes to dishes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120721220017) do
+ActiveRecord::Schema.define(:version => 20120721223607) do
 
   create_table "customers", :force => true do |t|
     t.integer  "table_number"
@@ -24,8 +24,12 @@ ActiveRecord::Schema.define(:version => 20120721220017) do
 
   create_table "dishes", :force => true do |t|
     t.integer  "menu_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.string   "name"
+    t.decimal  "price"
+    t.text     "description"
+    t.string   "image_url"
   end
 
   create_table "menus", :force => true do |t|
