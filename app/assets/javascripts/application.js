433fa3c57b70637cb66bec914361a24ae05270ec
Daniel Chiu <dwchiu@stanford.edu>
2012-07-22 05:42:48 -0700

    Put stuff back in app.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,14 @@
 //= require_tree
 
 
+$(function() {
+  if ($("#customers").length > 0) {
+    setTimeout(updateCustomers, 5000);
+  }
+});
+
+function updateCustomers() {
+  var restaurant_id = $("#customers").attr("data-id");
+  $.getScript("/customers.js?restaurant_id=" + restaurant_id );
+  setTimeout(updateCustomers, 5000);
+}
