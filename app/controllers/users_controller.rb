ff64d3bf08177ab5bb97ee3ce7f24e09bf98e3b0
Daniel Chiu <dwchiu@stanford.edu>
2012-07-21 18:10:32 -0700

    Hopefully REST JSON will work now.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_filter :verify_authenticity_token :only => :create
+  skip_before_filter :verify_authenticity_token, :only => :create
   # GET /users
   # GET /users.json
   def index
