681609004f7f9a1cf4839c4ac243cfc725311cfa
Daniel Chiu <dwchiu@stanford.edu>
2012-07-21 18:45:36 -0700

    More REST

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(params[:user])
-    logger.error "#{params[:user])}"
+    logger.error "#{params([:user])}"
 
     respond_to do |format|
       if @user.save
