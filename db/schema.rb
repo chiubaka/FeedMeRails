7bad0e625a33e490b19cf0829b4e9f7e04a7a2d2
Edward Lando <edwardlando8@gmail.com>
2012-07-21 22:18:48 -0700

    text for home page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120722042431) do
+ActiveRecord::Schema.define(:version => 20120722042759) do
 
   create_table "customers", :force => true do |t|
     t.integer  "table_number"
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(:version => 20120722042431) do
 
   create_table "dishes", :force => true do |t|
     t.integer  "menu_id"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at",                        :null => false
+    t.datetime "updated_at",                        :null => false
     t.string   "name"
     t.decimal  "price"
     t.text     "description"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20120722042431) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "rating",             :default => 0
   end
 
   create_table "menus", :force => true do |t|
