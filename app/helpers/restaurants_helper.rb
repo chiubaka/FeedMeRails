945ea845aa01353c12acecead51cabce8cb2f6f6
Edward Lando <edwardlando8@gmail.com>
2012-07-21 19:27:05 -0700

    home page styling

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,2 +1,13 @@
 module RestaurantsHelper
+  def resource_name
+      :restaurant
+  end
+
+  def resource
+      @resource ||= Restaurant.new
+  end
+
+  def devise_mapping
+      @devise_mapping ||= Devise.mappings[:restaurant]
+  end
 end
